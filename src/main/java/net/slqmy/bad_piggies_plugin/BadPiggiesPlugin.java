bc6895b8bb3610b06ca0dadac371fcd2c80dda66
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-03-06 19:09:20 +0000

    Initial commit

---
+++ src/main/java/net/slqmy/bad_piggies_plugin/BadPiggiesPlugin.java
@@ -0,0 +1,17 @@
+package net.slqmy.bad_piggies_plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BadPiggiesPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
