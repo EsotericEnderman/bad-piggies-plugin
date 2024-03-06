cb05c9f98cdb789dbf1196bc047f0fdb140a5219
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-03-06 21:20:28 +0000

    Simplified code

--- src/main/java/net/slqmy/bad_piggies_plugin/listener/InstantTntPlaceListener.java
+++ src/main/java/net/slqmy/bad_piggies_plugin/listener/InstantTntPlaceListener.java
@@ -5,10 +5,6 @@ import net.slqmy.bad_piggies_plugin.manager.InstantTntManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.persistence.PersistentDataContainer;
-import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 
 public class InstantTntPlaceListener implements Listener {
@@ -21,15 +17,7 @@ public class InstantTntPlaceListener implements Listener {
 
     @EventHandler
     public void onTntPlace(@NotNull BlockPlaceEvent event) {
-        ItemStack tntItem = event.getItemInHand();
-
-        ItemMeta meta = tntItem.getItemMeta();
-
-        PersistentDataContainer dataContainer = meta.getPersistentDataContainer();
-
-        Boolean isInstantTnt = dataContainer.get(plugin.getInstantTntKey(), PersistentDataType.BOOLEAN);
-
-        if (Boolean.FALSE.equals(isInstantTnt)) {
+        if (!plugin.getInstantTntManager().isInstantTnt(event.getItemInHand())) {
             return;
         }
 
