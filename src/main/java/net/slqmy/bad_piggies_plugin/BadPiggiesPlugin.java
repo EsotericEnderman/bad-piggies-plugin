8d845b596575f751f247fc0ba25e7033f566969b
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-03-06 20:08:32 +0000

    Added instant TNT recipe

--- src/main/java/net/slqmy/bad_piggies_plugin/BadPiggiesPlugin.java
+++ src/main/java/net/slqmy/bad_piggies_plugin/BadPiggiesPlugin.java
@@ -1,9 +1,25 @@
 package net.slqmy.bad_piggies_plugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Arrays;
+
 public final class BadPiggiesPlugin extends JavaPlugin {
+
+    private NamespacedKey instantTntKey;
+
+    public NamespacedKey getInstantTntKey() {
+        return instantTntKey;
+    }
 
     @Override
     public void onEnable() {
@@ -13,5 +29,37 @@ public final class BadPiggiesPlugin extends JavaPlugin {
 
         config.options().copyDefaults();
         saveDefaultConfig();
+
+        boolean isInstantTntEnabled = config.getBoolean("features.instant-tnt.enabled");
+
+        if (isInstantTntEnabled) {
+            ItemStack instantTnt = new ItemStack(Material.TNT);
+
+            ItemMeta meta = instantTnt.getItemMeta();
+
+            PersistentDataContainer dataContainer = meta.getPersistentDataContainer();
+
+            instantTntKey = new NamespacedKey(this, "instant-tnt");
+
+            dataContainer.set(instantTntKey, PersistentDataType.BOOLEAN, true);
+
+            instantTnt.setItemMeta(meta);
+
+            Material[] plankTypes = Arrays.stream(Material.values()).filter((material) -> material.name().endsWith("PLANKS")).toArray(Material[]::new);
+
+            for (Material plankType : plankTypes) {
+                NamespacedKey recipeKey = new NamespacedKey(this, "instant-tnt-recipe-" + plankType.name().toLowerCase());
+
+                ShapedRecipe instantTntRecipe = new ShapedRecipe(recipeKey, instantTnt);
+
+                instantTntRecipe.shape("GPG", "PGP", "GPG");
+
+                instantTntRecipe.setIngredient('G', Material.GUNPOWDER);
+
+                instantTntRecipe.setIngredient('P', plankType);
+
+                Bukkit.addRecipe(instantTntRecipe);
+            }
+        }
     }
 }
