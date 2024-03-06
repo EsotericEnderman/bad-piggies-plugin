9857a89215ea6cee439a7d54bf70d769487b4b33
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-03-06 19:12:41 +0000

    Added config

--- src/main/java/net/slqmy/bad_piggies_plugin/BadPiggiesPlugin.java
+++ src/main/java/net/slqmy/bad_piggies_plugin/BadPiggiesPlugin.java
@@ -1,17 +1,17 @@
 package net.slqmy.bad_piggies_plugin;
 
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BadPiggiesPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getDataFolder().mkdir();
 
-    }
+        YamlConfiguration config = (YamlConfiguration) getConfig();
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+        config.options().copyDefaults();
+        saveDefaultConfig();
     }
 }
