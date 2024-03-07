fec0e39e97b870cf82add3f73a9af56bf1f1cf20
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-03-07 19:09:00 +0000

    Added entity util

---
+++ src/main/java/net/slqmy/bad_piggies_plugin/util/EntityUtil.java
@@ -0,0 +1,47 @@
+package net.slqmy.bad_piggies_plugin.util;
+
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.util.BoundingBox;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EntityUtil {
+
+    public static @NotNull List<Block> getTouchedBlocks(@NotNull Entity entity) {
+        BoundingBox box = entity.getBoundingBox();
+
+        box.expand(0.1F);
+
+        double minX = box.getMinX();
+        double minY = box.getMinY();
+        double minZ = box.getMinZ();
+
+        double maxX = box.getMaxX();
+        double maxY = box.getMaxY();
+        double maxZ = box.getMaxZ();
+
+        World world = entity.getWorld();
+
+        List<Block> touchedBlocks = new ArrayList<>();
+
+        for (int x = (int) Math.floor(minX); x <= Math.floor(maxX); x++) {
+            for (int y = (int) Math.floor(minY); y <= Math.floor(maxY); y++) {
+                for (int z = (int) Math.floor(minZ); z <= Math.floor(maxZ); z++) {
+                    touchedBlocks.add(
+                            world.getBlockAt(
+                                    x,
+                                    y,
+                                    z
+                            )
+                    );
+                }
+            }
+        }
+
+        return touchedBlocks;
+    }
+}
